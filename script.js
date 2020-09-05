89243fe5d9f5def0eb2f6e144d973321a091f477
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-09-05 20:57:15 +0300

    05.09

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+/* исходное значение калькулятора (для одностворчатых окон) */
+let calc_func = 0.007831425253;
 
 /* кнопки становятся активными при нажатии на кнопку  */
 let btnContainer = document.getElementById("myBtnContainer");
@@ -18,36 +20,44 @@ function l_image(a) {
 }
 
 
-/*  Функция подсчета стоимости изделий  */
+/*  функция подсчета стоимости изделий  */
+function btn_calc_func(btn_calc_value){
+    calc_func = btn_calc_value;
+}
+
 function calc() {
     //получаем ссылку на элемент Select (Тип изделия)
-    var productType = document.getElementById('productType');
+    var productType = calc_func;
+    console.log(productType);
     //получаем ссылку на элемент input (Размеры)
     var valueLen = document.getElementById('valueLen');
     var valueWid = document.getElementById('valueWid');
     //получаем ссылку на элемент span, в него будем писать стоимость
     var result = document.getElementById('result');
     var price = 0;
-    price += parseInt(productType.options[productType.selectedIndex].value);
     //price += (is_html.checked == true) ? parseInt(is_html.value) : 0;
-    price = parseInt(valueLen.value * valueWid.value * productType.value).toFixed(2);
+    price = parseInt(valueLen.value * valueWid.value * productType).toFixed(2);
 
     result.innerHTML = price;
 }
 
 
 /*  Очищение формы при обновл.стр. */
-document.getElementById('valueLen').value = "1";
-document.getElementById('valueWid').value = "1";
-document.getElementById('productType').value = "0";
+document.getElementById('valueLen').value = "";
+document.getElementById('valueWid').value = "";
 
 
+/*  смена названия при */
+document.querySelector('.f5').addEventListener('click', preview);
 
-
-document.querySelector('.f5').addEventListener('click',preview);
- 
-function preview(e){
+function preview(e) {
     let box = e.target.closest('.box');
-    if(!box) return;
+    if (!box) return;
     document.querySelector('.windowinfo').textContent = box.children[0].textContent;
+}
+
+
+function cislo() {
+    if (event.keyCode < 48 || event.keyCode > 57)
+        event.returnValue = false;
 }
